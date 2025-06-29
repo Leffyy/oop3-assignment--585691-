1f1a89df89ab216e0bc59356fe814392256b81fa
Dary Delis <585691@student.inholland.nl>
2025-06-29 02:19:58 +0200

    add @NonNull annotation to addResourceHandlers parameter and update resource handler for image serving

--- src/main/java/com/example/moviewatchlist/config/WebConfig.java
+++ src/main/java/com/example/moviewatchlist/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.example.moviewatchlist.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     private String imagesPath;
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         // This allows the frontend to access images via URLs like:
         // http://localhost:8080/movie-images/Inception_0.jpg
         registry.addResourceHandler("/movie-images/**")
